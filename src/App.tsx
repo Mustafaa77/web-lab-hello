8931d65c88725919668fa70e349dda6b135ea75c
Mustafa Dürü <mustafa.duruu7@gmail.com>
2026-03-03 13:42:05 +0300

    chore: initial project setup with Vite React + TS

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Mustafa Dürü</p>
+      <p>Ogrenci No: [235541012]</p>
+      <p>Üniversite: Fırat Üniversitesi</p>
+    </div>
+  )
+}
+
+export default App
